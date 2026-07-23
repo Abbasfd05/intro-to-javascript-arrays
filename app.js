cb7aa3e9587c8346819c99a80c81ceecc561544d
Abbasfd05 <abbasfadhel2005abfd@gmail.com>
2026-07-23 10:34:24 +0300

    ok

--- app.js
+++ app.js
@@ -20,3 +20,14 @@ movies.forEach((movie, index) => {
   if(index === 2) console.log("This is the third movie:");
   console.log(movie);
 });
+for (let idx = 0; idx < movies.length; idx++) {
+  console.log(`${idx + 1}. ${movies[idx]}`);
+}
+function printMsg(movie) {
+const msg=`I like watching the movie ${movie}`;
+return msg;
+}
+console.log(printMsg("The Conjuring"));
+movies.forEach(function (movie) {
+  console.log('this is the movie ' + movie); 
+});
