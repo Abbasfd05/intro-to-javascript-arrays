2d7b8876313da2694f312048d88acd672101463b
Abbasfd05 <abbasfadhel2005abfd@gmail.com>
2026-07-23 09:35:52 +0300

    forEach lesson

--- app.js
+++ app.js
@@ -3,3 +3,20 @@ const movies=["TheConjuring","backrooms","Train to busan"];
 for (let i = 0; i < movies.length; i++) {
   console.log(movies[i]);
 }
+movies.push('Parasite', 'Dune');
+// movies is ['Barbie', 'Arrival', 'Get Out', 'Parasite', 'Dune']
+movies.pop();
+// movies is ['Barbie', 'Arrival', 'Get Out', 'Parasite"]
+const removedMovie = movies.pop();
+// movies is ['Barbie', 'Arrival', 'Get Out']
+// removedMovie is 'Parasite'
+for (let movie of movies) {
+  console.log(movie);
+}
+movies.forEach((movie) => {
+  console.log(movie)
+});
+movies.forEach((movie, index) => {
+  if(index === 2) console.log("This is the third movie:");
+  console.log(movie);
+});
